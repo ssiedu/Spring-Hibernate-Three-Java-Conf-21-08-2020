bcb39ac5a688cc2ff1e9a98585ab5afe1c1c9a24
manoj <manojs121@rediffmail.com>
2020-09-03 10:12:28 +0530

    03-09

--- src/main/java/com/ssi/entities/Course.java
+++ src/main/java/com/ssi/entities/Course.java
@@ -2,24 +2,34 @@ package com.ssi.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Range;
 
 @Entity
 public class Course {
 	@Id
+	
 	@Min(value=100, message="Code Cant Be Less Than 100")
 	private int code;
-	
+
+	@Pattern(regexp="[a-zA-Z]*", message="Only Alphabates Allowed")
+	@Size(min=5,max=10,message="title must be between 5-10 chars long")
 	private String title;
+
+	@Size(min=3,max=15,message="subject must be between 3-15 chars long")
 	private String subject;
+	
 	@Range(min=1, max=12, message="Duration Must Be Between 1-12 Only")
 	//@Min(value=1, message="Code")
 	//@Max(12)
 	private int duration;
+	
+	@Min(value=1000, message="Fees Cant Below 1000")
 	private int fees;
+	
 	public int getCode() {
 		return code;
 	}
